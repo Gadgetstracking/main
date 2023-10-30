1b9ed29893c7822faafacfcd526534af1126fcf8
Gadgetstracking <148624682+Gadgetstracking@users.noreply.github.com>
2023-10-30 11:07:21 +0200

    Create results.js

---
+++ results.js
@@ -0,0 +1,22 @@
+
+function trackingInfo(){
+    
+    var verify = document.getElementById("emailVerify").value;
+    
+       
+            var infoLay = document.getElementById("InfoLay");
+            var mainLay = document.getElementById("MainLay");
+            
+            
+            infoLay.style.display=("block");
+            mainLay.style.display=("none");
+    
+    
+    
+        if (verify=="masele"){
+            alert("this is masele")
+        }
+        
+    
+    
+}
