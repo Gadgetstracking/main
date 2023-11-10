dfc6850807e5f68c31f69e586126a8433665a564
Gadgetstracking <148624682+Gadgetstracking@users.noreply.github.com>
2023-11-10 10:42:20 +0200

    Update results.js

--- results.js
+++ results.js
@@ -53,6 +53,17 @@ if (username=="mokhurebryn@gmail.com"){
         moreInfo.style.display=("block");   
     }    
 
+if (username=="lenyatsotebo@gmail.com"){
+
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/W/2/u1Tz5l-d/huawei-enjoy-20-se.jpg";
+        
+        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 863084057524845</font><br/>  Model: Enjoy 20 SE (PPA-AL20)<br/>Brand: HUAWEI<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=("Description Full:<br/>Model Code: Peppa-AL20E<br/>IMEI: 863084057524845<br/>S/N:<font color=#ff0000> HBDNU21123210135</font><br/>Item Code: 51096GUL<br/>Offer Code: OFFE00261486<br/>Width	76.9 mm<br/>Height	165.7 mm<br/>Thickness	9.3 mm<br/>Weight	206.0 g<br/>Display Type	LCD_IPS<br/>Diagonal	6.67 inches<br/>Resolution	1080x2400 px<br/>Screen-to-body ratio	84.3 %<br/>Pixel density	395 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch, 60Hz, 2.5D curved glass screen ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");   
+    }    
     
     
 }
