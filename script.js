d51c06da6015b914dcb154b02f9e49706f348188
Gadgetstracking <148624682+Gadgetstracking@users.noreply.github.com>
2024-01-24 14:37:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+function run(){
+
+    var valuee=document.getElementById("tef").value;
+
+    if (valuee=="sudo_fetch_history"){
+         alert("fetch history");
+     }
+      else{
+      alert("To access more features subscribe to a plan");
+      }
+
+}
+
+
+
+
+function binance(){
+window.location.href=("BinanceMessages.html");
+}
+
+function orangeMoney(){
+window.location.href=("OrangeMoney.html");
+}
+
+function trustWallet(){
+window.location.href=("TrustWallet.html");
+}
+
+function conv(){
+window.location.href=("suspectConversation.html");
+}
+function conv(){
+window.location.href=("home.html");
+}
+
+
+
+
+
+
